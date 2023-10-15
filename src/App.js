85d6ef15cb75545c8095343e6aa2d6b6822b4394
Samie Tahir <badshayahoopakistan@gmail.com>
2023-10-14 23:52:06 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import Navbar from "./Comonents/Navbar";
+function App() {
+ 
+  return (
+    <>
+      <Navbar
+        link={{ link1: "Custom/Web", link2: "Home", link3: "About" }}
+      />
+      
+      
+    </>
+  );
+}
+
+export default App;
